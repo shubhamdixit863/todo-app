173ea74095f61fc98b019444843ba3d0ac90c78f
Shubham Dixit <shubham@saptharushi.com>
2026-05-12 17:29:37 +0530

    Input by number

--- src/components/ReduxComponent.jsx
+++ src/components/ReduxComponent.jsx
@@ -1,15 +1,25 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from '../slices/counterSlice'
+import { decrement, increment ,incrementByAmount} from '../slices/counterSlice'
+import { useState } from 'react'
 
 
 function ReduxComponent(params) {
      const count = useSelector((state) => state.counter.value)
   const dispatch = useDispatch()
+  const [state,setState]=useState(0);
 
   function incrementValue(){
     dispatch(increment())
   }
 
+  function increMEntByNumber(){
+    dispatch(incrementByAmount(state))
+  }
+
+  function setInput(event){
+    setState(parseInt(event.target.value));
+
+  }
     return <>
         <div>
       <div>
@@ -27,6 +37,15 @@ function ReduxComponent(params) {
           Decrement
         </button>
       </div>
+
+      <input type='text' onChange={setInput}></input>
+
+        <button
+          aria-label="Increment value"
+          onClick={increMEntByNumber}
+        >
+          Increment By Number
+        </button>
     </div>
     
     </>
